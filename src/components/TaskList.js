02be1d8b01f025f7aa5112c76566b7b6496574da
Miluche <Meshack.cheruiyot@student.moringaschool.com>
2023-01-14 09:42:19 +0300

    Added remove task functionality.

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import Task from "./Task";
 
 function TaskList({ tasks }) {
+  const [currentTasks, setCurrentTasks] = useState(tasks);
+
+  function removeTask(text) {
+    setCurrentTasks(currentTasks.filter((task) => {
+      return task.text !== text;
+    }));
+    return;
+  }
+
+
   return (
     <div className="tasks">
       {/* display a list of tasks using Task component */}
-      {tasks.map((task) => {
-        return <Task text={task.text} category={task.category} />
+      {currentTasks.map((task) => {
+        return <Task text={task.text} category={task.category} removeTask={removeTask} />
       })}
     </div>
   );
