a1e0f255b951a3ab878f1d151cb05b2156da25af
Miluche <Meshack.cheruiyot@student.moringaschool.com>
2023-01-14 09:55:54 +0300

    Display category and set category dynamically.

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
-function CategoryFilter() {
+function CategoryFilter({ categories }) {
+  const [isSelected, setIsSelected] = useState("All");
+
+  function selectCategory(category) {
+    setIsSelected(category);
+    return;
+  }
+
   return (
     <div className="categories">
       <h5>Category filters</h5>
       {/* render <button> elements for each category here */}
+      {categories.map((category, index) => {
+        return <button className={isSelected === category ? "selected" : ""} key={index} onClick={() => {
+          selectCategory(category);
+          return
+        }}>{category}</button>
+      })}
     </div>
   );
 }
